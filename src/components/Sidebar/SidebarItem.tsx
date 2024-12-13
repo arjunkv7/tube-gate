dbb2c8152840eaba920c26db4d78380f3a9eae85
Arjun <arjunkvarju7@gmail.com>
2024-12-13 11:10:48 +0530

    added menu themes dinamicaly

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import SidebarDropdown from "@/components/Sidebar/SidebarDropdown";
 import { usePathname } from "next/navigation";
+import * as HeroIcons from "@heroicons/react/24/outline"; // For outline icons
 
 const SidebarItem = ({ item, pageName, setPageName }: any) => {
   const handleClick = () => {
@@ -11,6 +12,14 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
   };
 
   const pathname = usePathname();
+  
+  HeroIcons
+// Resolve the Heroicon dynamically
+// const IconComponent = item.icon ? HeroIcons["Home"] : null;
+const IconComponent = item.icon
+  ? HeroIcons[`${item.icon.charAt(0).toUpperCase()}${item.icon.slice(1)}Icon`]
+  : null;
+
 
   const isActive = (item: any) => {
     if (item.route === pathname) return true;
@@ -31,9 +40,12 @@ const SidebarItem = ({ item, pageName, setPageName }: any) => {
           className={`${isItemActive ? "bg-graydark dark:bg-meta-4" : ""} group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4`}
         >
           {/* <img src={`${item.icon}`}></img> */} 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-          </svg>
+          </svg> */}
+           {IconComponent && (
+          <IconComponent className="h-6 w-6 text-current" aria-hidden="true" />
+        )}
           {/* {item.icon} */}
           {item.label}
           {item.children && (
